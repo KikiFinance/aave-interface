e7f4e6391bf7cdcf3657179430ae47f96b8fcedd
dino <dino@ldxes.com>
2025-06-08 08:40:57 +0800

    update meta

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -9,19 +9,19 @@ type MetaProps = {
 };
 
 export function Meta({ title, description, imageUrl, timestamp }: MetaProps) {
-  const formattedTitle = `lend - ${title}`;
+  const formattedTitle = `${title}`;
   return (
     <Head>
       <title>{formattedTitle}</title>{' '}
       <meta name="description" content={description} key="description" />
-      <meta property="og:title" content={`lend - ${title}`} key="title" />
+      <meta property="og:title" content={`${title}`} key="title" />
       <meta property="og:description" content={description} key="ogdescription" />
       {imageUrl && <meta property="og:image" content={imageUrl} key="ogimage" />}
       {imageUrl && <meta name="twitter:image" content={imageUrl} key="twitterimage" />}
       {imageUrl && (
         <meta name="twitter:image:alt" content={`aave governance image`} key="twitteralt" />
       )}
-      <meta name="twitter:site" content="@AaveAave" key="twittersite" />
+      <meta name="twitter:site" content="@KiKi_Finance" key="twittersite" />
       <meta
         property="twitter:card"
         content={imageUrl ? 'summary_large_image' : 'summary'}
@@ -35,8 +35,8 @@ export function Meta({ title, description, imageUrl, timestamp }: MetaProps) {
         key="keywords"
         content="Decentralized Finance, DeFi, lending, borrowing, stablecoins, Ethereum, assets, erc-20, smart contracts, open finance, trustless"
       />
-      <link rel="apple-touch-icon" href="/aave_180.png" />
-      <meta name="apple-mobile-web-app-title" content={`Aave`} />
+      <link rel="apple-touch-icon" href="/favicon.ico" />
+      <meta name="apple-mobile-web-app-title" content={title} />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     </Head>
